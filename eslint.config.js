11af610a97c82ca48a60644685f093cece2974f5
vikram(wsl) <vikramjit360@gmail.com>
2024-10-04 20:32:20 +0530

    chore: update eslint config to latest version

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/prop-types": 0,
+      "no-unused-vars": 1,
+      "react/react-in-jsx-scope": 0,
+    },
+  },
+];
