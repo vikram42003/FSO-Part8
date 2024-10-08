09785f0041b6d12019527352208876d7a872ba3c
vikram(wsl) <vikramjit360@gmail.com>
2024-10-08 14:30:19 +0530

    8.19 - feat: add book filtering functionality

--- src/views/Books.jsx
+++ src/views/Books.jsx
@@ -1,27 +1,43 @@
 import { useQuery } from "@apollo/client";
 import { getAllBooks } from "../graphql/queries";
+import { useState } from "react";
 
 const Books = () => {
+  const [genre, setGenre] = useState("all");
   const { loading, error, data } = useQuery(getAllBooks);
 
   if (loading) return <div>Loading books data...</div>;
 
   if (error) return <div>Some error occured...</div>;
 
+  const tableStyle = {
+    width: "100%",
+    textAlign: "left",
+  };
+
   const books = data.allBooks;
+  const genreSet = new Set();
+  books.forEach((book) => book.genres.forEach((g) => genreSet.add(g)));
+  const genreList = [...genreSet];
+
+  const filteredBooks = genre === "all" ? books : books.filter((book) => book.genres.includes(genre));
 
   return (
     <div>
       <h2>books</h2>
 
-      <table>
+      <p>
+        selected genre - <b>{genre}</b>
+      </p>
+
+      <table style={tableStyle}>
         <tbody>
           <tr>
             <th></th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -30,6 +46,17 @@ const Books = () => {
           ))}
         </tbody>
       </table>
+
+      <div>
+        <button type="button" onClick={() => setGenre("all")} style={genre === "all" ? { borderColor: "blue" } : {}}>
+          All
+        </button>
+        {genreList.map((g) => (
+          <button key={g} type="button" onClick={() => setGenre(g)} style={genre === g ? { borderColor: "blue" } : {}}>
+            {g}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
